43909e5af8ae604da165a393e1313d8e4947107d
Karim Darra <karimdarra17@gmail.com>
2024-02-25 20:11:26 -0500

    T-2.1 Implemented Logout Button on frontend

--- frontend/finvue/src/components/navbar/navbar.js
+++ frontend/finvue/src/components/navbar/navbar.js
@@ -8,13 +8,14 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/">FinVue</Link>
+        <Link to="/transaction-dashboard">FinVue</Link>
       </div>
       <ul className="nav-links">
         <li><Link to="/transaction-dashboard">Transaction Dashboard</Link></li>
         <li><Link to="/stocks">Stocks</Link></li>
         <li><Link to="/budget">Budget</Link></li>
         <li><Link to="/savings">Savings</Link></li>
+        <li><Link to="/">Logout</Link></li>
       </ul>
     </nav>
   );
